1c94e43a11c84e272e7ddc0ed29e0d0e2344807d
Kittipob <lchokunl@hotmail.com>
2017-12-01 14:20:06 +0700

    add redcircle

--- core/src/com/mygdx/game/ArrowGame.java
+++ core/src/com/mygdx/game/ArrowGame.java
@@ -1,22 +1,24 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class ArrowGame extends ApplicationAdapter {
-	SpriteBatch batch;
+public class ArrowGame extends Game {
+	public SpriteBatch batch;
 
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		setScreen(new GameScreen(this));
 	}
 
 	@Override
-	public void render () {
+    public void render () {
 		super.render();
 	}
 	
---
+++ core/src/com/mygdx/game/GameScreen.java
@@ -0,0 +1,26 @@
+package com.mygdx.game;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class GameScreen extends ScreenAdapter {
+	private ArrowGame arrowGame;
+	private Texture redcircleImg;
+	private Texture uparrowImg;
+	 
+    public GameScreen(ArrowGame arrowGame) {
+        this.arrowGame = arrowGame;
+        redcircleImg = new Texture("red-circle.png");
+        uparrowImg = new Texture("up-arrow.png");
+        
+    
+    }
+	public void render (float delta) {
+		SpriteBatch batch = arrowGame.batch;
+        batch.begin();
+        batch.draw(redcircleImg, 350, 450);
+        batch.draw(uparrowImg, 350, 450);
+        batch.end();
+	}
+
+}
