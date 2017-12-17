6890a204925786aa5b5cd606dcdcaebe410de07b
Kittipob <lchokunl@hotmail.com>
2017-12-17 20:16:14 +0700

    add other picture

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -2,16 +2,26 @@ package com.mygdx.game;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import java.util.Random;
 
 public class GameScreen extends ScreenAdapter {
 	private ArrowGame arrowGame;
 	private Texture redcircleImg;
+	private Texture bluecircleImg;
 	private Texture uparrowImg;
+	private Texture downarrowImg;
+	private Texture leftarrowImg;
+	private Texture rightarrowImg;
 	 
     public GameScreen(ArrowGame arrowGame) {
         this.arrowGame = arrowGame;
+        Random rand = new Random();
         redcircleImg = new Texture("red-circle.png");
+        bluecircleImg = new Texture("blue-circle.png");
         uparrowImg = new Texture("up-arrow.png");
+        downarrowImg = new Texture("down-arrow.png");
+        leftarrowImg = new Texture("left-arrow.png");
+        rightarrowImg = new Texture("right-arrow.png");
         
     
     }
