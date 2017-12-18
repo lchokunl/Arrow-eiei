a8c5860f8e05dcfde6098bab4d88e04f0b236395
Kittipob <lchokunl@hotmail.com>
2017-12-18 10:42:59 +0700

    debug score

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -19,6 +19,7 @@ public class GameScreen extends ScreenAdapter {
 	private int randCircle;
 	private int randArrow;
 	private int score;
+	private boolean isPlaying;
 	private BitmapFont font;
 	
     public GameScreen(ArrowGame arrowGame) {
@@ -31,41 +32,92 @@ public class GameScreen extends ScreenAdapter {
         rightarrowImg = new Texture("right-arrow.png");
         background = new Texture("background.jpg");
         score = 0;
-        //font = new BitmapFont();
-    
+        font = new BitmapFont();
+        isPlaying = false;
     }
 	public void render (float delta) {
 		SpriteBatch batch = arrowGame.batch;
         batch.begin();
         batch.draw(background, 0, 0);
         Random rand = new Random();
-        if(Gdx.input.isKeyJustPressed(Keys.UP) || Gdx.input.isKeyJustPressed(Keys.DOWN) || Gdx.input.isKeyJustPressed(Keys.LEFT) || Gdx.input.isKeyJustPressed(Keys.RIGHT)){
-        	randCircle = rand.nextInt(2)+1;
-        	randArrow = rand.nextInt(4)+1;
-        }
-        if(randCircle == 1) {
-        	batch.draw(redcircleImg, 350, 450);
-        }
-        else if(randCircle == 2) {
-        	batch.draw(bluecircleImg, 350, 450);
-        }
-        if(randArrow == 1) {
-        	batch.draw(uparrowImg, 415, 515);
-        }
-        else if(randArrow == 2) {
-        	batch.draw(downarrowImg, 415, 515);
-        }
-        else if(randArrow == 3) {
-        	batch.draw(leftarrowImg, 415, 515);
-        }
-        else if(randArrow == 4) {
-        	batch.draw(rightarrowImg, 415, 515);
-        }
-        if(Gdx.input.isKeyJustPressed(Keys.UP) && ((randArrow == 1 && randCircle == 2) || (randArrow == 2 && randCircle == 1))) {
-        	score += 10;
-        }
         
-        //font.draw(batch, "Score : " + score, 700, 60);
+        if (!isPlaying) {
+        	//if(Gdx.input.isKeyJustPressed(Keys.UP) || Gdx.input.isKeyJustPressed(Keys.DOWN) || Gdx.input.isKeyJustPressed(Keys.LEFT) || Gdx.input.isKeyJustPressed(Keys.RIGHT)){
+        		randCircle = rand.nextInt(2) + 1;
+        		randArrow = rand.nextInt(4) + 1;
+        		isPlaying = true;
+        	//}
+        }
+        else {
+        	if(randCircle == 1) {
+        		batch.draw(redcircleImg, 350, 450);
+        	}
+        	else if(randCircle == 2) {
+        		batch.draw(bluecircleImg, 350, 450);
+        	}
+        
+        	if(randArrow == 1) {
+        		batch.draw(uparrowImg, 415, 515);
+        	}
+        	else if(randArrow == 2) {
+        		batch.draw(downarrowImg, 415, 515);
+        	}
+        	else if(randArrow == 3) {
+        		batch.draw(leftarrowImg, 415, 515);
+        	}
+        	else if(randArrow == 4) {
+        		batch.draw(rightarrowImg, 415, 515);
+        	}
+        	
+        	if (randCircle == 1) {
+        		if (randArrow == 1) {
+        			if( Gdx.input.isKeyJustPressed(Keys.DOWN)) {
+        				score += 10;
+        				isPlaying = false;
+        			}
+        		} else if (randArrow == 2) {
+        			if( Gdx.input.isKeyJustPressed(Keys.UP)) {
+        				score += 10;
+        				isPlaying = false;
+        			}
+        		} else if (randArrow == 3) {
+        			if( Gdx.input.isKeyJustPressed(Keys.RIGHT)) {
+        				score += 10;
+        				isPlaying = false;
+        			}
+        		} else if (randArrow == 4) {
+        			if( Gdx.input.isKeyJustPressed(Keys.LEFT)) {
+        				score += 10;
+        				isPlaying = false;
+        			}
+        		}
+        	} else if (randCircle == 2) {
+        		if (randArrow == 1) {
+        			if( Gdx.input.isKeyJustPressed(Keys.UP)) {
+        				score += 10;
+        				isPlaying = false;
+        			}
+        		} else if (randArrow == 2) {
+        			if( Gdx.input.isKeyJustPressed(Keys.DOWN)) {
+        				score += 10;
+        				isPlaying = false;
+        			}
+        		} else if (randArrow == 3) {
+        			if( Gdx.input.isKeyJustPressed(Keys.LEFT)) {
+        				score += 10;
+        				isPlaying = false;
+        			}
+        		} else if (randArrow == 4) {
+        			if( Gdx.input.isKeyJustPressed(Keys.RIGHT)) {
+        				score += 10;
+        				isPlaying = false;
+        			}
+        		}	
+        	}
+        }
+        System.out.println("score: " + score);
+        font.draw(batch, "Score : " + score, 550, 60);
+        font.getData().setScale(5f);
         
         batch.end();
 	}
